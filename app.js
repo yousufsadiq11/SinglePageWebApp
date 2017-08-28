80007ef83a80bc882214eed1bd9cae68a9c0d1ca
yousufsadiq11@gmail.com <yousufsadiq11@gmail.com>
2017-08-28 18:37:56 -0400

    module 2

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 'use strict';
 angular.module('AngularTest',[])
 .controller('controllertest',['$scope','$filter',function($scope,$filter){
-
-$scope.name="";
-$scope.convert=function(){
+var test=$scope;
+test.name="";
+test.convert=function(){
   var z=$filter('uppercase');
-  $scope.name=z($scope.name);
+  test.name=z(test.name);
 
 };
 }]);
